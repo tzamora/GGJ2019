014cedbda3d8c60ed286d46406c8d066de4c1cbe
tzamora <tzamora@gmail.com>
2019-01-21 15:18:21 -0600

    tea time test

---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using matnesis.TeaTime;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject body;
+
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RotationRoutine();
+    }
+
+    void RotationRoutine()
+    {
+
+        this.tt().Loop((ttHandler handler) => {
+            body.transform.Rotate(speed * Vector3.right * Time.deltaTime);
+        });
+
+    }
+}
