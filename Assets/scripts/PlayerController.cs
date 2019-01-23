24e54a3a4ff450ffee9d96bdd1507a80104466e9
tzamora <tzamora@gmail.com>
2019-01-22 23:51:28 -0600

    add multiplayer management

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using matnesis.TeaTime;
+using InControl;
 
 public class PlayerController : MonoBehaviour
 {
@@ -11,26 +12,67 @@ public class PlayerController : MonoBehaviour
 
     public AudioClip starSound;
 
+    GameInputActions gameInputActions; 
+
     // Start is called before the first frame update
     void Start()
     {
+        GameInputBinding();
+
+        InputRoutine();
+
         RotationRoutine();
 
         SoundsRoutine();
     }
 
+    void Update()
+    {
+        if (gameInputActions.attack.WasPressed)
+        {
+            print("botonzaso");
+        }
+    }
+
+    void GameInputBinding()
+    {
+        gameInputActions = new GameInputActions();
+
+        gameInputActions.Left.AddDefaultBinding(Key.LeftArrow);
+        gameInputActions.Left.AddDefaultBinding(InputControlType.DPadLeft);
+
+        gameInputActions.Right.AddDefaultBinding(Key.RightArrow);
+        gameInputActions.Right.AddDefaultBinding(InputControlType.DPadRight);
+
+        gameInputActions.attack.AddDefaultBinding(Key.Space);
+        gameInputActions.attack.AddDefaultBinding(InputControlType.Action1);
+    }
+
+    void InputRoutine() {
+
+        this.tt().Loop((ttHandler handler) => {
+
+            var inputDevice = InputManager.ActiveDevice;
+            //print(inputDevice.LeftStickX);
+            body.transform.Rotate(speed * Vector3.right * Time.deltaTime * inputDevice.LeftStickX);
+
+
+        });
+
+    }
+
     void RotationRoutine()
     {
 
-        this.tt().Loop((ttHandler handler) => {
-            body.transform.Rotate(speed * Vector3.right * Time.deltaTime);
-        });
+        //this.tt().Loop((ttHandler handler) => {
+        //    body.transform.Rotate(speed * Vector3.right * Time.deltaTime);
+        //});
 
     }
 
     void SoundsRoutine()
     {
-        this.tt().Add(2, (ttHandler handler) => {
+        this.tt().Add(5, (ttHandler handler) => {
 
             SoundManager.Get.PlayClip(starSound, false);
 
---
+++ Assets/scripts/input/MultiplayerInControl.cs
@@ -0,0 +1,101 @@
+﻿
+// @matnesis
+// 2015/10/23 11:44 PM
+
+
+using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+using matnesis.TeaTime;
+using InControl;
+
+
+
+public class MultiplayerInControl : MonoBehaviour
+{
+	public List<GamePlayerInput> players = null; // Players list
+	public int deviceCount = 0; // Current controls connected
+
+	[Header("Last Refresh")]
+	public List<GamePlayerInput> playersWithoutDevice;
+	public List<InputDevice> busyDevices;
+	public List<InputDevice> freeDevices;
+	public GamePlayerInput keyboardPlayer = null;
+
+
+	void Start()
+	{
+		// When a new usb device is detected, the controls are reassigned
+		// for the players without devices
+		this.tt("RefreshDevices").Add(1, () =>
+		{
+			if (InputManager.Devices.Count != deviceCount)
+			{
+				deviceCount = InputManager.Devices.Count;
+				AssignFreeDevicesToFreePlayers();
+			}
+		})
+		.Repeat();
+
+
+		// On 'Space' or 'Enter', a free tank will be activated for the
+		// keyboard.
+		this.tt("KeyboardPlayerDetection").Loop((ttHandler t) =>
+		{
+			if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.KeypadEnter))
+			{
+				keyboardPlayer = players.Where(
+				                     p => p.inputDevice == null ||
+				                     !InputManager.Devices.Contains(p.inputDevice)).FirstOrDefault();
+
+
+				// If there is a free lion
+				if (keyboardPlayer != null)
+				{
+					keyboardPlayer.gameObject.SetActive(true);
+
+					keyboardPlayer.useKeyboard = true;
+					keyboardPlayer.forceActiveDevice = true;
+					keyboardPlayer.SetInputDevice(null, -1);
+
+					AssignFreeDevicesToFreePlayers();
+				}
+			}
+		})
+		.Wait();
+		
+		// Look for all the compatible players, except the active device
+		players = FindObjectsOfType<GamePlayerInput>().ToList();
+	}
+
+
+	// Assign free devices to free players.
+	void AssignFreeDevicesToFreePlayers()
+	{
+		// Queries
+		playersWithoutDevice = 
+			players.Where(p => !p.useKeyboard && (p.inputDevice == null || !InputManager.Devices.Contains (p.inputDevice) )).ToList();
+		
+		busyDevices = players.Where(p => p.inputDevice != null).Select(d => d.inputDevice).ToList();
+
+		freeDevices = InputManager.Devices.Where(d => !busyDevices.Contains(d)).ToList();
+
+
+		// Players & Devices
+		for (int i = 0; i < playersWithoutDevice.Count; i++)
+		{
+			// Free devices are the limit
+			if (i >= freeDevices.Count)
+			{
+				// Turn off players without device
+				if (playersWithoutDevice[i] != keyboardPlayer)
+					playersWithoutDevice[i].gameObject.SetActive(false);
+
+				continue;
+			}
+
+			playersWithoutDevice[i].gameObject.SetActive(true);
+			playersWithoutDevice[i].SetInputDevice(freeDevices[i], i);
+		}
+	}
+}
