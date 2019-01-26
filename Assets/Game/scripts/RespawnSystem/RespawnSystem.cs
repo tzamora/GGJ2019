4b2291c74276cc589f04c61ccb05dd1780d6d45f
Andrés Villalobos <andres.villalobos@sweetrush.com>
2019-01-26 14:00:14 -0600

    Respawn System !

---
+++ Assets/Game/scripts/RespawnSystem/RespawnSystem.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnSystem : MonoBehaviour
+{
+    public RespawnSignal[] players;
+
+    void Update()
+    {
+        for (int i = 0; i < players.Length; i++)
+        {
+            var player = players[i];
+            if (player.isReadyToSpawn)
+            {
+                player.isReadyToSpawn = false;
+                player.transform.position = Vector3.zero;
+            }
+        }
+    }
+
+}
