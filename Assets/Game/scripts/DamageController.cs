e32d3dcf2b50e09560e77d13f0f473fe1ff9eeb8
tzamora <tzamora@gmail.com>
2019-01-26 03:01:01 -0600

    collision

--- Assets/Game/scripts/DamageController.cs
+++ Assets/Game/scripts/DamageController.cs
@@ -9,13 +9,6 @@ public class DamageController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        bodyCollider.OnEnter += (Collider collider) =>
-        {
-            var player = bodyCollider.gameObject.GetComponent<PlayerController>();
-            if (player)
-            {
-                player.KillPlayerRoutine();
-            }
-        };
+        
     }
 }
