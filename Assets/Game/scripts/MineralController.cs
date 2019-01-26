9305d1633fcfa31dc0fc02dda6601bc26f30146e
tzamora <tzamora@gmail.com>
2019-01-26 08:01:39 -0600

    mineral

--- Assets/Game/scripts/MineralController.cs
+++ Assets/Game/scripts/MineralController.cs
@@ -7,18 +7,21 @@ public class MineralController : MonoBehaviour
     [Header("Settings")]
     public int amount;
 
+    public bool isInside;
+
     public TriggerController bodyCollider;
 
     // Start is called before the first frame update
     void Update()
     {
-        if (bodyCollider.isColliding) {
+        if (bodyCollider.isColliding && isInside) {
 
-            print("entrando");
+            // isInside = true;
+            // print("entrando");
 
             var player = bodyCollider.gameObject.GetComponent<PlayerController>();
-
-            print(player);
+            // no tiene un player controller 
+            // print(player);
             
             if (player && player.gamePlayerInput.actions.attack.WasPressed) {
                 print("botonazo");
