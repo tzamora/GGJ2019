97321d07889e54d058b5d42096fa851b342a2089
Andrés Villalobos <andres.villalobos@sweetrush.com>
2019-01-26 16:40:48 -0600

    MineralPrefab works

---
+++ Assets/Game/scripts/MineralSystem/MineralTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MineralTrigger : MonoBehaviour
+{
+    public List<Collider> colliders;
+
+    void OnTriggerEnter(Collider other)
+    {
+        // Only players
+        var player = other.GetComponent<PlayerController>();
+        if (!player) return;
+
+        colliders.Add(other);
+    }
+}
