c7c28d45b355308b4330ad498cbb4afdefe4b571
DESKTOP-0RE2D7R\eladi <eladiozro@gmail.com>
2019-01-27 15:20:14 -0400

    Refreshing Home

--- Assets/Game/scripts/MineralController.cs
+++ Assets/Game/scripts/MineralController.cs
@@ -2,32 +2,81 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MineralController : MonoBehaviour
-{
-    [Header("Settings")]
+public class MineralController : MonoBehaviour {
+    [Header ("Settings")]
     public int amount;
 
     public AudioClip recolectSound;
 
     public TriggerController bodyCollider;
 
+    public float glowSpeed = 1;
+
+    public Color sourceColor = Color.white;
+
+    public List<GameObject> Shapes = new List<GameObject> ();
+
     // Start is called before the first frame update
-    void Update()
-    {
+
+    bool emissionEnabled = false;
+    Material materialGlow;
+
+    void Start () { GetShape (); }
+
+    void Update () {
+
         if (bodyCollider.isColliding) {
 
             foreach (var collider in bodyCollider.others) {
 
-                var player = collider.gameObject.GetComponent<PlayerController>();
-                
-                if (player && player.gamePlayerInput.actions.attack.WasPressed)
-                {
-                    player.Recolect(amount);
-                    Destroy(gameObject);
-                    SoundManager.Get.PlayClip(recolectSound, false);
+                var player = collider.gameObject.GetComponent<PlayerController> ();
+
+                if (player && player.gamePlayerInput.actions.attack.WasPressed) {
+                    player.Recolect (amount);
+                    Destroy (gameObject);
+                    SoundManager.Get.PlayClip (recolectSound, false);
                 }
 
             }
         }
+
+        if (emissionEnabled) EmissionGlow ();
+
     }
+
+    void GetShape () {
+
+        int r = Random.Range (0, Shapes.Count);
+
+        GameObject theShape = Instantiate (Shapes[r],
+            gameObject.transform.position, gameObject.transform.rotation);
+
+        theShape.name = "Obj";
+        theShape.transform.SetParent (gameObject.transform);
+
+        // print ("random was " + r);
+
+        if (r >= Shapes.Count - 3) {
+            emissionEnabled = true;
+            Destroy (GetComponent<Rotating> ());
+            materialGlow = theShape.GetComponent<MeshRenderer> ().material;
+            gameObject.transform.Translate (new Vector3 (0, -0.5f, 0));
+        }
+
+    }
+
+    void EmissionGlow () {
+
+        float emission = Mathf.PingPong (Time.time * glowSpeed, 1.3f);
+
+        Color baseColor = sourceColor;
+        // Replace this with whatever you want
+        // for your base color at emission level '1'
+
+        Color finalColor = baseColor * Mathf.LinearToGammaSpace (emission);
+
+        materialGlow.SetColor ("_EmissionColor", finalColor);
+
+    }
+
 }
---
+++ Assets/Game/scripts/other/Rotating.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotating : MonoBehaviour {
+
+    public Vector3 rotateSpeedValues = new Vector3 (100, 100, 100);
+    public float randomSizeIntensity = 0.1f;
+    // random start is away from capabilities
+    public bool setRandomSpeed;
+    public bool setRandomSize;
+    public bool startRandomRotation;
+
+    Vector3 rotateSpeed;
+
+    public void Start () {
+        //
+        if (setRandomSize) transform.localScale =
+            transform.localScale * Random.Range (1 - randomSizeIntensity,
+                1 + randomSizeIntensity);
+        //
+        if (setRandomSpeed) {
+            rotateSpeedValues.x = rotateSpeedValues.x * Random.Range (0.25f, 1);
+            rotateSpeedValues.y = rotateSpeedValues.y * Random.Range (0.25f, 1);
+            rotateSpeedValues.z = rotateSpeedValues.z * Random.Range (0.25f, 1);
+        }
+        //
+        if (startRandomRotation)
+            transform.localEulerAngles = rotateSpeedValues * Random.Range (0, 360);
+        //
+        rotateSpeed = rotateSpeedValues;
+        //
+    }
+
+    void Update () {
+
+        transform.Rotate (rotateSpeed * Time.deltaTime);
+
+    }
+}
